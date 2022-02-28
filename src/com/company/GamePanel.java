c99e8df22ed5d898ccde59b25da238af7fa52cf0
li <gs210755@reigate.ac.uk>
2022-02-28 20:53:26 +0000

    added power ups and sounds

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -22,21 +22,22 @@ public class GamePanel extends JPanel implements Runnable{
     //WORLD SETTINGS
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
+    public final int worldWidth = tileSize * maxWorldCol;  //can delete
+    public final int worldHeight = tileSize * maxWorldRow; //can delete
 
     // FPS
     int FPS = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    Sound sound = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    Thread gameThread;
+
+    //ENTITY AND OBJECT
     public Player player = new Player(this,keyH);
     public SuperObject obj[] = new SuperObject[10];
-
-
 
 
     // SET PLAYER DEFAULT POSITION
@@ -55,6 +56,8 @@ public class GamePanel extends JPanel implements Runnable{
     public void setupGame(){
 
         aSetter.setObject();
+
+        playMusic(0);
     }
 
     public void startGameThread() {
@@ -122,4 +125,21 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.dispose();
     }
+
+    public void playMusic (int i){
+        sound.setFile(i);
+        sound.play();
+        sound.loop();
+    }
+
+    public void stopMusic(){
+        sound.stop();
+    }
+
+    public void playSE (int i){
+        sound.setFile(i);
+        sound.play();
+    }
+
+
 }
