59877e38cc7ba082a53827f8f6c213a1a311ebc1
Max Burton-Phillips <MB211187@reigate.ac.uk>
2022-03-09 15:54:25 +0000

    Added pause state in

--- src/com/company/UI.java
+++ src/com/company/UI.java
@@ -10,6 +10,7 @@ public class UI {
 
 
     GamePanel gp;
+    Graphics2D g2;
     Font arial_40, arial_80B;
     BufferedImage keyImage;
     private static boolean messageOn = false;
@@ -37,6 +38,8 @@ public class UI {
     }
 
     public void draw(Graphics2D g2) {
+
+        this.g2 = g2;
         if (gameFinished == true) {
 
             g2.setFont(arial_40);
@@ -47,29 +50,27 @@ public class UI {
             int x;
             int y;
 
-            text="You found the treasure!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 - (gp.tileSize*3);
+            text = "You found the treasure!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 - (gp.tileSize * 3);
             g2.drawString(text, x, y);
 
-            text="Your Time is :" + dFormat.format(playTime)+ "!" ;
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + (gp.tileSize*4);
+            text = "Your Time is :" + dFormat.format(playTime) + "!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 + (gp.tileSize * 4);
             g2.drawString(text, x, y);
 
             g2.setFont(arial_80B);
             g2.setColor(Color.yellow);
-            text="Congratulations!";
-            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + (gp.tileSize*2);
+            text = "Congratulations!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 + (gp.tileSize * 2);
             g2.drawString(text, x, y);
 
             gp.gameThread = null;
-
-
 
 
         } else {
@@ -77,27 +78,57 @@ public class UI {
 
             g2.setFont(arial_40);
             g2.setColor(Color.white);
-            g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
-            g2.drawString("x  " + gp.player.hasKey, 74, 65);
-
-            //TIME
-            playTime +=(double) 1/60;
-            g2.drawString("Time: "+dFormat.format(playTime), gp.tileSize*11, 65);
-
-            //MESSAGE
-            if (messageOn == true) {
-
-                g2.setFont(g2.getFont().deriveFont(30F));
-                g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
-
-                messageCounter++;
 
 
-                if (messageCounter > 120) {
-                    messageCounter = 0;
-                    messageOn = false;
+            if (gp.gameState == gp.playState) {
+                g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
+                g2.drawString("x  " + gp.player.hasKey, 74, 65);
+
+                //TIME
+                playTime += (double) 1 / 60;
+                g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize * 11, 65);
+
+                //MESSAGE
+                if (messageOn == true) {
+
+                    g2.setFont(g2.getFont().deriveFont(30F));
+                    g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
+
+                    messageCounter++;
+
+
+                    if (messageCounter > 120) {
+                        messageCounter = 0;
+                        messageOn = false;
+
+                    }
                 }
             }
+            if (gp.gameState == gp.pauseState) {
+                drawPauseScreen();
+
+            }
+
+
         }
     }
+
+    public void drawPauseScreen() {
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN,80F));
+        String text = "PAUSED";
+        int x = getXforCenteredText(text);
+
+
+        int y = gp.screenHeight/2;
+
+        g2.drawString(text, x, y);
+    }
+    public int getXforCenteredText(String text){
+        int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+        int x = gp.screenWidth / 2 - length / 2;
+        return x;
+    }
 }
+
+
+
